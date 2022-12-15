52409bac95640ea55f10578c6581361d8d07e93e
Matt-DWAPS <matthieu.junge@outlook.com>
2022-12-15 15:14:32 +0100

    feature/CREATE_TRICKS_FORM  - add form

---
+++ src/Form/MovieType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Movie;
+use Doctrine\DBAL\Types\TextType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MovieType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('url', UrlType::class, [
+                'label' => "Ajouter l'Url embed de la vidéo que vous souhaitez attaché à la figure",
+                'required' => false,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Movie::class,
+        ]);
+    }
+}
